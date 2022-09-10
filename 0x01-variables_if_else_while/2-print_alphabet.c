ee66f09ea4b22b6785031eb67c80050871dd1751
Andree05 <anolorunfemi@gmail.com>
2022-09-10 12:58:58 -0700

    C lang

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -2,41 +2,23 @@
 
 /**
  *
- *  ** main - Prints combination of numbers
+ *  *  * main - Prints the alphabetic
  *
- *   **
+ *   *   *
  *
- *    ** Return: Always (Success)
+ *    *    * Return: Always (Success)
  *
- *     **/
+ *     *     */
 
 int main(void)
 
 {
 
-	int c, i;
+	char c;
 
-	for (c = '0'; c <= '9'; c++)
+	for (c = 'a'; c <= 'z'; c++)
 
-	{
-
-		for (i = '0'; i <= '9'; i++)
-
-		{
-
-			if (c < i)
-
-			{
-
-				putchar(c);
-
-				putchar(i);
-
-			}
-
-		}
-
-	}
+		putchar(c);
 
 	putchar('\n');
 
