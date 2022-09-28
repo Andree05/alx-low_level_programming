100dadefce5fab3fbd12205006f9999b42890980
Andree05 <anolorunfemi@gmail.com>
2022-09-28 04:15:06 -0700

    recursion

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -1,20 +1,23 @@
 #include "main.h"
+
 /**
- * _pow_recursion -Returns the value of x rasied to the power of y.
- * @x: the power to be raised.
- * @y: the power.
- * return: result
+ * _pow_recursion - Search a string for any of a set of bytes.
+ * @x: base
+ * @y: exposant
+ * Return: Pointer to the byte in `s` that matches one of the bytes in `accept`
+ * or NULL if no such byte is found.
  */
 
 int _pow_recursion(int x, int y)
 {
-	int result = x;
 
 	if (y < 0)
 		return (-1);
+	else if (y == 1)
+		return (x);
 	else if (y == 0)
 		return (1);
 
-	result *= _pow_recursion(x, y - 1);
-	return (result);
+	return (x * _pow_recursion(x, y - 1));
+
 }
